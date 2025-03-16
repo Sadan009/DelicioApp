208bb88aa57195b8934ec43f8d211276ef8fcbeb
Sadan009 <imamsadan009@gmail.com>
2025-03-16 16:23:41 +0530

    Connected react to redux

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 import { AiFillStar } from "react-icons/ai";
+import { useDispatch } from "react-redux";
+import { addToCart } from "../redux/slices/CartSlice";
 
 const FoodCard = ({ id, name, price, desc, rating, foodImg }) => {
+  
+
+  const dispatch = useDispatch();
+
   return (
     <div className="font-bold w-[250px] bg-white p-5 flex flex-col rounded-lg gap-2">
       <img
@@ -18,7 +24,11 @@ const FoodCard = ({ id, name, price, desc, rating, foodImg }) => {
         <span className="flex items-center gap-2">
           <AiFillStar className="text-yellow-400" /> {rating}
         </span>
-        <button className="py-1 px-2 text-white text-sm bg-green-500 hover:bg-green-600 rounded-lg">
+        <button 
+        onClick={() => {
+          dispatch(addToCart({id, name, price, rating, qty: 1}))
+        }}
+        className="py-1 px-2 text-white text-sm bg-green-500 hover:bg-green-600 rounded-lg">
           Add to cart
         </button>
       </div>
--- src/redux/Store.js
+++ src/redux/Store.js
@@ -2,9 +2,9 @@ import { configureStore } from "@reduxjs/toolkit";
 import CartSlice from "./slices/CartSlice";
 
 const Store = configureStore({
-    reducer: {
-        cart: CartSlice,
-    }
+  reducer: {
+    cart: CartSlice,
+  },
 });
 
 export default Store;
