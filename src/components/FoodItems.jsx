1815209f92d84d7501fa32e21916089e1dfff10f
Sadan009 <imamsadan009@gmail.com>
2025-03-17 19:15:44 +0530

    filter categories

--- src/components/FoodItems.jsx
+++ src/components/FoodItems.jsx
@@ -2,15 +2,25 @@ import React from "react";
 import FoodCard from "./FoodCard";
 import FoodData from "../assets/data/FoodData";
 import toast, { Toaster } from "react-hot-toast";
+import { useSelector } from "react-redux";
 
 const FoodItems = () => {
+  // to filter data first we use useSelector:
+  const category = useSelector((state) => state.category.category);
+
   const handleToast = (name) => toast.success(`${name} added to cart`);
 
   return (
     <>
       <Toaster position="top-center" reverseOrder={false} />
       <div className="flex flex-wrap  gap-8 justify-center">
-        {FoodData.map((food) => {
+        {FoodData.filter((food) => {
+          if (category === "All") {
+            return food;
+          } else {
+            return category === food.category;
+          }
+        }).map((food) => {
           return (
             <FoodCard
               key={food.id}
